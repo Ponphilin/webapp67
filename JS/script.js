893ce33ea96d39507e04fdc23469d151ff25ae73
Ponphilin <Mei24022547@gmail.com>
2024-09-04 11:33:10 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+console.log("hello javascript");
+console.log("สวัสดีวันพุธ"); /*ผลลัพธ์ใน Console */
+document.getElementById("text").innerHTML = "Ponphilin Channgarm"; /*เรียกใช้ ID ชื่อ Text*/
+window.alert("welcome to my page"); /*แสดงหน้าต่างแจ้งเตือน*/
